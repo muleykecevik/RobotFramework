def7be7dbf1d6a286bc638ef8388a20d89cfdb61
muleykecevik <muleyke.cevik@gmail.com>
2023-11-08 23:13:30 +0300

    08/11/23

---
+++ day02/01_While.py
@@ -0,0 +1,6 @@
+sayi1=10
+sayi2=20
+
+while sayi1 <= sayi2:
+    print(sayi1)
+    sayi+=1
---
+++ day02/02_For.py
@@ -0,0 +1,8 @@
+names= ["Yusuf", "Ahmet", "Akif", "Ayse"]
+
+for e in names:
+    print(e)
+
+#enumerate = once dizin sonra index alir
+for index, eleman in enumerate (names):
+    print(f"{index}: {eleman}")   
