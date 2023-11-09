ca4560a0fe467f986bafde8477beb1430c979eed
muleykecevik <muleyke.cevik@gmail.com>
2023-11-09 17:55:31 +0300

    09/11/23

---
+++ day02/07_Map_Filter.py
@@ -0,0 +1,21 @@
+isimler = ["Yusuf", "Ahmet", "Mehmet", "Yunus"]
+
+# Bu fonksiyon bool (boolean) deger dondurur
+def kontrol(degisken):
+    return 'u' in degisken
+
+
+#print("##########################")
+#print(list(map(kontrol, isimler)))
+# Ciktisi -> True, False, False, True
+
+#print("##########################")
+#print(list(filter(kontrol, isimler)))
+# Ciktisi -> Yusuf, Yunus
+"""
+if "u" in "Ahmet":
+    print(True)
+else:
+    print(False)
+"""
+print(kontrol("ahmet"))
